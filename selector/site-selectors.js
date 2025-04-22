3328f3fb41dc219929a6434525d6102811317f7f
Samuel Rath <samuel.rath.email@gmail.com>
2025-04-23 00:50:30 +1000

    added selector dan murphy australia

--- selector/site-selectors.js
+++ selector/site-selectors.js
@@ -2,13 +2,14 @@
  * Map of retailer hostnames to CSS selectors for scraping bottle data.
  * To support a new site, add its hostname and the corresponding selectors below.
  */
+// selectors/site‑selectors.js
 export const SITE_SELECTORS = {
-    // Example for example-retailer.com:
-    // 'example-retailer.com': {
-    //   name:    '.product-title',      // Selector for the bottle name element
-    //   price:   '.price-amount',       // Selector for the price element
-    //   image:   '.product-image img',   // Selector for the bottle image tag
-    //   vintage: '.product-vintage'      // Optional: selector for the vintage/year
-    // },
+    'www.danmurphys.com.au': {
+      name:  '#pdp-productname > div',
+      price: '#mat-tab-content-1-0 .pack__right',
+      image: 'img.zoom__image'   
+    }
   };
+  
+  
   
